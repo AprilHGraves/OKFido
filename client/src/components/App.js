60f928227087084a2dcce154167bdeb902782185
christie <cbrandao18@gmail.com>
2019-07-29 13:35:21 -0400

    style main splash header, rotate through images

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import Nav from './Nav'
+import Splash from './splash'
 function App() {
   return (
     <div className="App">
-      Hello Waldo
-
-      <Route path="/" component={Nav}/>
+      <Route path="/" component={Nav} />
+      <Route path="/" component={Splash}/>
     </div>
   );
 }
--- client/src/components/splash.jsx
+++ client/src/components/splash.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 
 class Splash extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.bgImageClasses = [
+      'splash-container splash-bg-1', 
+      'splash-container splash-bg-2', 
+      'splash-container splash-bg-3',
+    ];
+
+    this.bgColorClasses = [
+      'splash-container splash-bg-1-color',
+      'splash-container splash-bg-2-color',
+      'splash-container splash-bg-3-color',
+    ]
+
+    this.bgNum = 0;
+    this.state = {
+      currentBg: this.bgImageClasses[this.bgNum]
+    }
+  }
+
+  componentDidMount(){
+    this.bgInterval = setInterval(() => {
+      this.rotateBG();
+    }, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.bgInterval);
+  }
+
+  rotateBG() {
+    this.setState({ currentBg: this.bgColorClasses[this.bgNum % 3] })
+    this.bgNum++;
+    setTimeout( () => {
+      this.setState({ currentBg: this.bgImageClasses[this.bgNum % 3] })
+    }, 100)
+  }
+
   render() {
     return (
-      <div className="splash-container">
-
+      <div className={this.state.currentBg}>
+        <h1>ADOPTION DESERVES <br /> BETTER</h1>
+        <p>
+          On OkAdopt, you're more than just a photo. You have
+          a home to share, time to play, and a heart big enough
+          to care for another living creature. Get noticed for your
+          great choice in dog toys, and your large backyard. Because
+          you deserve a dog to cuddle with.
+        </p>
       </div>
     )
   }
