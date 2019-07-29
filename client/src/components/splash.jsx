483a2223b6bedd45da33b05ea6d5bd4b63e1982e
christie <cbrandao18@gmail.com>
2019-07-29 12:00:02 -0400

    create stylesheets, add script to change the scss to css

---
+++ client/src/components/splash.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Splash extends React.Component {
+  render() {
+    return (
+      <div className="splash-container">
+
+      </div>
+    )
+  }
+}
+
+export default Splash;
