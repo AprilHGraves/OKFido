d3adcacca2d4d25666bc4651cdcd24fd289218d2
christie <cbrandao18@gmail.com>
2019-07-30 17:54:01 -0400

    add home nav and style hover on dog index

--- client/src/components/home/nav.jsx
+++ client/src/components/home/nav.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 class Nav extends React.Component {
   render(){
     return (
       <div className="navigation">
-
+        <div className="nav-left">
+          <Link to="/">
+            <i className="fas fa-dog"></i>
+          </Link>
+          <Link to='/browse'>Browse Matches</Link>
+        </div>
+        <div className="nav-right">
+          <Link to='/messages'>
+            <i className="fas fa-comment"></i>
+          </Link>
+          <Link>Logout</Link>
+        </div>
       </div>
     )
   }
