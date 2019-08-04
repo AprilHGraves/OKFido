7c62ffe14a489390bd0f5fb15d09c61f21ab4a51
christie <cbrandao18@gmail.com>
2019-08-04 16:01:20 -0400

    dockerize the project

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,5 +2,6 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   SECRET_OR_KEY: process.env.SECRET_OR_KEY,
   DOG_KEY: process.env.DOG_KEY,
-  ADOPT_KEY: process.env.ADOPT_KEY
+  ADOPT_KEY: process.env.ADOPT_KEY,
+  ADOPT_SECRET: process.env.ADOPT_SECRET
 }
